b18365a7e4cb6eccdc70c03856bd366b7fa097d0
Rerumu <25379555+Rerumu@users.noreply.github.com>
2023-11-05 14:50:29 -0500

    Fix Luau NaN tests

--- dev-test/tests/luau_assert.lua
+++ dev-test/tests/luau_assert.lua
@@ -64,9 +64,9 @@ end
 local loaded = {}
 local linked = {}
 
-local LUA_NAN_ARITHMETIC = -(0 / 0)
-local LUA_NAN_CANONICAL = -(0 / 0)
-local LUA_NAN_DEFAULT = -(0 / 0)
+local LUA_NAN_ARITHMETIC = 0 / 0
+local LUA_NAN_CANONICAL = 0 / 0
+local LUA_NAN_DEFAULT = 0 / 0
 local LUA_INFINITY = math.huge
 
 local function is_number_equal(lhs, rhs)
